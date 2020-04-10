5c28837608fb6773ee7d919ba79fb1fe008b41d8
_rdx98 <elder_louzada@outlook.com>
2020-04-10 00:56:28 -0300

    initial commit | header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../../assets/creditas-branco.png'
+import './styles.css'
+
+export default function Header () {
+    return <header>
+        <div className="logo">
+            <img src={logo} alt="logo-creditas"/>
+        </div>
+        <ul>
+            <li>
+                Ajuda
+            </li>
+        </ul>
+    </header>
+}
